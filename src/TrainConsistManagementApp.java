8df5b9a444b84ddd068493a734268751019bbd58
Ashraf0909 <skashraf1025@gmail.com>
2026-04-17 09:49:57 +0530

    UC8: Filter Passenger Bogies Using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class tcm {
+public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
         List<String> bogies = new ArrayList<>();
